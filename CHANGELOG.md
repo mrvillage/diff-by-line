620b6262989c567f4377d2b0de2533f53e8ff275
mrvillage <josef@mrvillage.dev>
2024-08-19 15:04:23 -0400

    Initial creation

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+<!-- next-header -->
+
+## [Unreleased] - ReleaseDate
+
+<!-- next-url -->
+
+[unreleased]: https://github.com/mrvillage/diff-by-line/compare/v0.0.0...HEAD
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <file1> <file2>", args[0]);
+        std::process::exit(1);
+    }
+    let file1 = std::fs::read_to_string(&args[1]).unwrap();
+    let file2 = std::fs::read_to_string(&args[2]).unwrap();
+    for (i, (line1, line2)) in file1.lines().zip(file2.lines()).enumerate() {
+        if line1 != line2 {
+            println!("Line {}", i + 1);
+            println!("{}: {}", args[1], line1);
+            println!("{}: {}", args[2], line2);
+            println!();
+        }
+    }
+}
